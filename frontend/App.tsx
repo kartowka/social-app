c0d56691919a5d7f30a274c77b62b13f175d8817
kartowka <antonfley@gmail.com>
2022-06-02 16:18:25 +0300

    default frontPage rerouted to src/router/index

---
+++ frontend/App.tsx
@@ -0,0 +1,6 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import Router from '@router/index'
+export default function App() {
+	return <Router />
+}
