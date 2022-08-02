db9f27430e83c57f01d6b17eefc3594fe8d5c133
kartowka <antonfley@gmail.com>
2022-08-02 21:46:30 +0300

    custom button for homescreen

--- frontend/src/components/SocialButton.tsx
+++ frontend/src/components/SocialButton.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react'
-import { View, Text, TouchableHighlight, StyleSheet } from 'react-native'
+import { View, Text, TouchableHighlight } from 'react-native'
 import { SvgUri } from 'react-native-svg'
+import style from '@components/SocialButtonWrapper'
 
-const SocialButton: FC<{ text: string; name: string; onPress: () => void; color: string; textColor: string }> = ({ text, name, onPress, color, textColor }) => {
+const SocialButton: FC<{ text: string; name: string; onPress: (serviceName: string) => void; color: string; textColor: string }> = ({ text, name, onPress, color, textColor }) => {
 	const socialSign = () => {
 		if (name === 'Facebook') {
 			return 'https://www.svgrepo.com/show/138943/facebook.svg'
@@ -14,46 +15,15 @@ const SocialButton: FC<{ text: string; name: string; onPress: () => void; color:
 		return ''
 	}
 	return (
-		<TouchableHighlight style={name != '' ? null : [style.containerSignup, { backgroundColor: color, borderColor: color }]} onPress={() => onPress()} activeOpacity={0.2} underlayColor={color}>
-			<View style={name != '' ? style.containerSocial : null}>
-				<View style={name != '' ? style.containerInnerSocial : null}>
+		<View style={name != '' ? style.containerOuterSocial : [style.containerOuterSignup, { backgroundColor: color, borderColor: color }]}>
+			<TouchableHighlight onPress={() => onPress(name)} activeOpacity={0.2} underlayColor={color}>
+				<View style={name != '' ? style.containerInnerSocial : style.containerInnerSignup}>
 					<SvgUri width='40%' height='100%' uri={socialSign()} style={name != '' ? style.iconPlacement : null} />
 					<Text style={[style.text, { color: textColor }, name != '' ? null : { textAlign: 'center' }]}>{text}</Text>
 				</View>
-			</View>
-		</TouchableHighlight>
+			</TouchableHighlight>
+		</View>
 	)
 }
 
 export default SocialButton
-
-const style = StyleSheet.create({
-	containerSignup: {
-		marginBottom: 10,
-		borderWidth: 10,
-		borderRadius: 60,
-		marginLeft: 30,
-		marginRight: 30,
-	},
-	containerSocial: {
-		borderColor: 'grey',
-		borderWidth: 1,
-		borderRadius: 60,
-		marginLeft: 30,
-		marginRight: 30,
-		marginBottom: 10,
-	},
-	containerInnerSocial: {
-		borderColor: '#000000',
-		borderRadius: 60,
-		borderWidth: 10,
-		flexDirection: 'row',
-	},
-	text: {
-		fontFamily: 'Optima',
-		fontSize: 15,
-	},
-	iconPlacement: {
-		marginLeft: -50,
-	},
-})
---
+++ frontend/src/components/SocialButtonWrapper.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native'
+
+const style = StyleSheet.create({
+	containerOuterSignup: {
+		marginBottom: 10,
+		padding: 10,
+		borderRadius: 60,
+		marginLeft: 30,
+		marginRight: 30,
+	},
+	containerOuterSocial: {
+		borderColor: 'grey',
+		borderWidth: 1,
+		borderRadius: 60,
+		marginLeft: 30,
+		marginRight: 30,
+		marginBottom: 10,
+	},
+	containerInnerSocial: {
+		flexDirection: 'row',
+		borderColor: '#000000',
+		borderRadius: 60,
+		padding: 10,
+	},
+	containerInnerSignup: {},
+	text: {
+		fontSize: 15,
+	},
+	iconPlacement: {
+		marginLeft: -50,
+	},
+})
+export default style
