04b75195ee7380d67d00acb983c17cf47f0ca359
kartowka <antonfley@gmail.com>
2022-08-02 21:41:14 +0300

    configuration file for expo client

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
 import Router from '@router/index'
+import 'react-native-gesture-handler'
+
 export default function App() {
 	return <Router />
 }
