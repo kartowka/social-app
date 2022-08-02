3e0395f07dc6231d55e43928a717eadc8f0f8f36
kartowka <antonfley@gmail.com>
2022-08-02 21:53:02 +0300

    homescreen style file

---
+++ frontend/src/screens/guest/home/styles.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native'
+const style = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	imageBackground: {
+		flex: 1,
+		justifyContent: 'center',
+	},
+	descriptionContainer: {
+		marginBottom: -30,
+		marginTop: 300,
+		marginLeft: 100,
+		marginRight: 100,
+		alignContent: 'center',
+		alignItems: 'center',
+	},
+	descriptionText: {
+		fontSize: 20,
+		color: '#ffffff',
+		textAlign: 'center',
+	},
+})
+
+export default style
