fc7ad313f723c7ccaf0ad31f446dbcddb82cfb14
kartowka <antonfley@gmail.com>
2022-08-02 21:56:23 +0300

    style file for signup complete page

---
+++ frontend/src/screens/guest/signup/styles/signupCompleteWrapper.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+const signupCompleteWrapper = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#000000',
+	},
+	animationContainer: {
+		height: '50%',
+	},
+	text: {
+		fontWeight: '600',
+		fontSize: 20,
+		paddingTop: 10,
+		textAlign: 'center',
+		color: '#1fdf64',
+	},
+})
+export default signupCompleteWrapper
