367e4e63f84f48b17535cdabd442bc0bc00a7096
kartowka <antonfley@gmail.com>
2022-08-02 21:51:23 +0300

    index file contain exports

---
+++ frontend/src/screens/guest/index.tsx
@@ -0,0 +1,6 @@
+import Login from '@screens/guest/signup/Login'
+import Signup from '@screens/guest/signup/Signup'
+import SignupComplete from '@screens/guest/signup/SignupComplete'
+import HomeScreen from '@screens/guest/home/HomeScreen'
+
+export { Login, Signup, SignupComplete, HomeScreen }
