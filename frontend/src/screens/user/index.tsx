fc71ea98be0c7cf6409a161c1d92e9f9966e7223
kartowka <antonfley@gmail.com>
2022-08-02 21:57:48 +0300

    index file contain exports

---
+++ frontend/src/screens/user/index.tsx
@@ -0,0 +1,7 @@
+import ChatScreen from '@screens/user/chat/ChatScreen'
+import HomeScreenLoggedIn from '@screens/user/home/HomeScreenLoggedIn'
+import PostsScreen from '@screens/user/posts/PostsScreen'
+import SettingsScreen from '@screens/user/Settings/SettingsScreen'
+import MyPosts from '@screens/user/posts/MyPosts'
+
+export { ChatScreen, HomeScreenLoggedIn, PostsScreen, SettingsScreen, MyPosts }
