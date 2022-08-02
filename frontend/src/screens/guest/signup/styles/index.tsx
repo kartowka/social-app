12638edcbaa11a42697c4bd5ec10d4f2b3dd70c5
kartowka <antonfley@gmail.com>
2022-08-02 21:55:04 +0300

    index file contain exports

---
+++ frontend/src/screens/guest/signup/styles/index.tsx
@@ -0,0 +1,5 @@
+import loginWrapper from './loginWrapper'
+import signupWrapper from './signupWrapper'
+import signupCompleteWrapper from './signupCompleteWrapper'
+
+export { loginWrapper, signupWrapper, signupCompleteWrapper }
