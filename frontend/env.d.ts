f2e264a4d67cc317d6fbe967dd810817770ff027
kartowka <antonfley@gmail.com>
2022-08-02 21:42:04 +0300

    typescript env resolver

---
+++ frontend/env.d.ts
@@ -0,0 +1,16 @@
+declare module '@env' {
+	export const BACKEND_SERVICE_URL: string
+	export const GOOGLE_CLIENT_ID: string
+	export const FACEBOOK_CLIENT_ID: string
+}
+/**
+ *  this file contain type declaration for the .env file, Typescript fix.
+ *  to make this .env file works  you should declare inside babal.config.js those lines under plugin
+ *  [
+ * 	'module:react-native-dotenv',
+ * 	{
+ * 		moduleName: 'react-native-dotenv',
+ * 	},
+ * ],
+ * then declare your variables under .env in root directory API_KEY="URL"
+ */
