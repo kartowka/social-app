ef776c5ebf3c3475180df0cd044731d7ca277269
imso3k <imso3k@gmail.com>
2022-08-02 22:22:04 +0300

    exporter

---
+++ backend/src/errors/index.ts
@@ -0,0 +1,5 @@
+import BadRequestError from './bad-request'
+import ForbiddenError from './forbidden'
+import NotFoundError from './not-found'
+import UnAuthenticatedError from './unauthenticated'
+export { BadRequestError, NotFoundError, UnAuthenticatedError, ForbiddenError }
