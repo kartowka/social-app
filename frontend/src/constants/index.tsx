50845c12da83cb5790191b8137e72baa2f8a5f62
kartowka <antonfley@gmail.com>
2022-08-02 21:49:23 +0300

    custom constant variables

---
+++ frontend/src/constants/index.tsx
@@ -0,0 +1,47 @@
+import Icons from '@expo/vector-icons/MaterialCommunityIcons'
+import { Platform, StyleSheet } from 'react-native'
+
+const BORDERTEST = StyleSheet.create({
+	border: {
+		borderColor: 'red',
+		borderWidth: 1,
+	},
+})
+const defaultScreenOptionStack = ({ route }: any) => ({
+	headerStyle: { backgroundColor: '#000000' },
+	headerTintColor: '#ffffff',
+})
+
+const defaultScreenOptionBottomTab = ({ route }: any) => ({
+	headerStyle: { backgroundColor: '#000000' },
+	headerShown: false,
+	headerTintColor: '#1fdf64',
+	tabBarStyle: { backgroundColor: '#000000' },
+	tabBarIcon: () => {
+		let iconName: any
+
+		if (route.name === 'Home') {
+			iconName = 'home'
+		} else if (route.name === 'Chat') {
+			iconName = 'chat'
+		} else if (route.name === 'Posts') {
+			iconName = 'forum'
+		} else if (route.name === 'Settings') {
+			iconName = 'cog'
+		}
+
+		// You can return any component that you like here!
+		return <Icons name={iconName} size={25} color={'#1fdf64'} />
+	},
+	tabBarActiveTintColor: '#1fdf64',
+	tabBarInactiveTintColor: '#909090',
+})
+const serverURL = 'http://192.168.1.212:3000'
+const endpoints = {
+	facebook: serverURL + '/facebook',
+	google: serverURL + '/google',
+}
+
+const PLATFORM_PADDING = Platform.OS === 'ios' ? 45 : 25
+
+export { defaultScreenOptionStack, defaultScreenOptionBottomTab, PLATFORM_PADDING, BORDERTEST, endpoints }
